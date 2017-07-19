0682ee0f97cb0833a15e74e4822190b3a8330a91
RangaPrakashs <prakash.mortal@gmail.com>
2017-07-19 14:59:07 +0530

    Added RandomNumberGenerator

---
+++ Engine/RandomNumberGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class RandomNumberGenerator
+    {
+        private static Random _random = new Random();
+
+        public static int NumberInBetween(int minimumValue, int maximiumValue)
+        {
+            return _random.Next(minimumValue, maximiumValue+1);
+        }
+    }
+}
